8f9ada119c877f3e5ce4574af9bca541cf7cf0d8
wonderlove <mmordante@gmail.com>
2021-02-20 15:22:44 -0500

    Fifth: Fixed Overflow of Cards

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //remove this part with the data received from esp
   List<Disturbance> placeholder = [
     Disturbance(level: 'low', time: "5:30 am"),
+    Disturbance(level: 'medium', time: "5:34 am"),
+    Disturbance(level: 'medium', time: "5:34 am"),
+    Disturbance(level: 'medium', time: "5:34 am"),
     Disturbance(level: 'medium', time: "5:34 am"),
   ];
 
@@ -55,7 +59,8 @@ class _HomeState extends State<Home> {
           Container(
             height: 292.0,
             width: 502.0,
-            child: Column(
+            child: ListView(
+              //edit this when ble is connected and the data is received
               children: placeholder.map((disturbance) => ListCard(disturbance: disturbance)).toList(),
             ),
             decoration: const BoxDecoration(
@@ -72,159 +77,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
-// class flutterab extends StatefulWidget {
-//   @override
-//   _flutterabState createState() => _flutterabState();
-// }
-//
-// class _flutterabState extends State<flutterab> {
-//   FlutterBle _flutterBlue = FlutterBle.instance;
-//
-//   StreamSubscription _scanSubscription;
-//   Map<DeviceIdentifier, ScanResult> scanResults = new Map();
-//   bool isScanning = false;
-//
-//   StreamSubscription _stateSubscription;
-//   BluetoothState state = BluetoothState.unknown;
-//
-//   BluetoothDevice device;
-//
-//   bool get isConnected => (device != null);
-//   StreamSubscription deviceConnection;
-//   StreamSubscription deviceStateSubscription;
-//   List<BluetoothService> services = new List();
-//   Map<Guid, StreamSubscription> valueChangedSubscriptions = {};
-//   BluetoothDeviceState deviceState = BluetoothDeviceState.disconnected;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Immediately get the state of FlutterBle
-//     _flutterBlue.state.then((s) {
-//       setState(() {
-//         state = s;
-//       });
-//     });
-//     // Subscribe to state changes
-//     _stateSubscription = _flutterBlue.onStateChanged().listen((s) {
-//       setState(() {
-//         state = s;
-//       });
-//     });
-//   }
-//
-//   @override
-//   void dispose() {
-//     _stateSubscription?.cancel();
-//     _stateSubscription = null;
-//     _scanSubscription?.cancel();
-//     _scanSubscription = null;
-//     deviceConnection?.cancel();
-//     deviceConnection = null;
-//     super.dispose();
-//   }
-//   _startScan() {
-//     _scanSubscription = _flutterBlue
-//         .scanForScale(timeout: const Duration(seconds: 10 ))
-//         .listen((scanResult) {
-//       print('localName: ${scanResult.advertisementData.localName}');
-//       print(
-//           'manufacturerData: ${scanResult.advertisementData.manufacturerData}');
-//       print('serviceData: ${scanResult.advertisementData.serviceData}');
-//       setState(() {
-//         scanResults[scanResult.device.id] = scanResult;
-//       });
-//     }, onDone: _stopScan);
-//
-//     setState(() {
-//       isScanning = true;
-//     });
-//   }
-//
-//   _startScanB() {
-//     _scanSubscription = _flutterBlue
-//         .scanForBPM(timeout: const Duration(seconds: 5))
-//         .listen((scanResult) {
-//       print('localName: ${scanResult.advertisementData.localName}');
-//       print(
-//           'manufacturerData: ${scanResult.advertisementData.manufacturerData}');
-//       print('serviceData: ${scanResult.advertisementData.serviceData}');
-//       setState(() {
-//         scanResults[scanResult.device.id] = scanResult;
-//       });
-//     }, onDone: _stopScan);
-//
-//     setState(() {
-//       isScanning = true;
-//     });
-//   }
-//
-//   _stopScan() {
-//     _scanSubscription?.cancel();
-//     _scanSubscription = null;
-//     setState(() {
-//       isScanning = false;
-//     });
-//   }
-//
-//   _connect(BluetoothDevice d) async {
-//     device = d;
-//     // Connect to device
-//     deviceConnection = _flutterBlue
-//         .connect(device, timeout: const Duration(seconds: 4))
-//         .listen(
-//       null,
-//       onDone: _disconnect,
-//     );
-//
-//     _connect(BluetoothDevice d) async {
-//       device = d;
-//       // Connect to device
-//       deviceConnection = _flutterBlue
-//           .connect(device, timeout: const Duration(seconds: 4))
-//           .listen(
-//         null,
-//         onDone: _disconnect,
-//       );
-//
-//       // Update the connection state immediately
-//       device.state.then((s) {
-//         setState(() {
-//           deviceState = s;
-//         });
-//       });
-//
-//       // Subscribe to connection changes
-//       deviceStateSubscription = device.onStateChanged().listen((s) {
-//         setState(() {
-//           deviceState = s;
-//         });
-//         if (s == BluetoothDeviceState.connected) {
-//           device.discoverServices().then((s) {
-//             setState(() {
-//               services = s;
-//             });
-//           });
-//         }
-//       });
-//     }
-//
-//     _disconnect() {
-//       // Remove all value changed listeners
-//       valueChangedSubscriptions.forEach((uuid, sub) => sub.cancel());
-//       valueChangedSubscriptions.clear();
-//       deviceStateSubscription?.cancel();
-//       deviceStateSubscription = null;
-//       deviceConnection?.cancel();
-//       deviceConnection = null;
-//       setState(() {
-//         device = null;
-//       });
-//     }
-//
-//   //
-//   //   Widget build(BuildContext context) {
-//   //   return Container();
-//   // }
-// }
